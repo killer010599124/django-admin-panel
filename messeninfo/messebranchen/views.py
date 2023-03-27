4116a71947e862669845804c0aa8bf208e51aa1f
super-developer-king <killerrampage0105@gmail.com>
2023-03-27 16:18:38 +0900

    ui

--- messeninfo/messebranchen/views.py
+++ messeninfo/messebranchen/views.py
@@ -11,6 +11,7 @@ from django.templatetags.static import static
 
 def HomeView(request):
     
+ 
     alphabet = string.ascii_uppercase
     context = {}
     context["dataset"] = TradeFair.objects.all().order_by('title').values()
@@ -23,17 +24,7 @@ def CategoryView(request, cats):
     return render(request, 'trade_fair.html', {'cats':cats, 'category_posts':category_posts})
 
 def NewBranchenView(request):
-    # if request.method == 'POST':
-    #     form = ImageForm(request.POST, request.FILES)
-    #     print (form)
-    #     if form.is_valid():
-    #         form.save()
-    #         # Get the current instance object to display in the template
-    #         img_obj = form.instance
-    #         return render(request, 'newbranchen.html', {'form': form, 'img_obj': img_obj})
-    # else:
-    #     form = ImageForm()
-    # return render(request, 'newbranchen.html', {'form': form})
+    
    
     if request.method == 'POST':
         
@@ -82,20 +73,7 @@ def NewBranchenView(request):
 
 def EditBranchenView(request, cats): 
     
-    # category_posts = TradeFair.objects.filter(category_id=cats)
-    # if request.method == 'POST':
-    #     updateData = TradeFair.objects.get(category_id=cats)
-    #     updateData.category_id = request.POST.get('category')
-    #     updateData.title = request.POST.get('title')
-    #     updateData.description = request.POST.get('description')
-    #     updateData.image1 = request.FILES.get('image1')
-    #     updateData.image2 = request.FILES.get('image2')
-        
-    #     # updateData=TradeFair(category_id=request.POST.get('category'),title=request.POST.get('title'),
-    #     #     description=request.POST.get('description'),image1=request.FILES.get('image1'),image2=request.FILES.get('image2'))
-    #     updateData.save()
-    #     return redirect('home')
-    # return render(request, 'editbranchen.html', {'cats':cats, 'category_posts':category_posts})
+
     
     category_posts = Branchen.objects.filter(b_id=cats)
     if request.method == 'POST':
